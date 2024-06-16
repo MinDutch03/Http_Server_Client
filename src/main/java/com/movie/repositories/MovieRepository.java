d1c88c702c2d48b4e4a33954e8bd537f382acfb1
Duc Nguyen <nguyenminhduc890@gmail.com>
2024-06-16 18:32:32 +0700

    Update MovieRepository.java

--- src/main/java/com/movie/repositories/MovieRepository.java
+++ src/main/java/com/movie/repositories/MovieRepository.java
@@ -3,7 +3,5 @@ package com.movie.repositories;
 import com.movie.entities.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
-
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 }
